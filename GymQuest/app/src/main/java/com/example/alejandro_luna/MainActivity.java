b0dba81ef298e29e913b2c48300836c307061c23
alvarohghg <lvaro.berjillos@gmail.com>
2023-04-07 15:36:34 +0100

    List fixed + user-day creation
    Now we can list any collection from firebase and also when an user login for the first time a profile with email and routines for each day is created in the collection "user-day"

--- GymQuest/app/src/main/java/com/example/alejandro_luna/MainActivity.java
+++ GymQuest/app/src/main/java/com/example/alejandro_luna/MainActivity.java
@@ -3,32 +3,89 @@ package com.example.alejandro_luna;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.alejandro_luna.Login;
+import com.example.alejandro_luna.R;
+import com.example.alejandro_luna.RecyclerViewAdapter;
+import com.example.alejandro_luna.UserProfile;
+import com.example.alejandro_luna.planification;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.ArrayList;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button logoutButton,planificationButton;
 
+    private Button logoutButton, planificationButton;
     private ImageView userProfile;
-    ArrayList<String> exercises = new ArrayList<>();
 
     RecyclerViewAdapter recyclerViewAdapter;
+
+    String currentUserEmail;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currentUserEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+
+        // Create user-day collection and add document with user's email
+        //only for the first time
+        
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference userRef = database.getReference("user-day");
+
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                boolean userExists = false;
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    Map<String, Object> userData = (Map<String, Object>) userSnapshot.getValue();
+                    if (userData != null && userData.get("email") != null && userData.get("email").equals(currentUserEmail)) {
+                        userExists = true;
+                        break;
+                    }
+                }
+                if (!userExists) {
+                    DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+                    DatabaseReference userDayRef = rootRef.child("user-day").push();
+                    userDayRef.child("email").setValue(currentUserEmail);
+                    userDayRef.child("monday").setValue("");
+                    userDayRef.child("tuesday").setValue("");
+                    userDayRef.child("wednesday").setValue("");
+                    userDayRef.child("thursday").setValue("");
+                    userDayRef.child("friday").setValue("");
+                    userDayRef.child("saturday").setValue("");
+                    userDayRef.child("sunday").setValue("");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("Firebase", "Error al leer los datos", databaseError.toException());
+            }
+        });
 
 
 
@@ -39,6 +96,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(new Intent(MainActivity.this, planification.class));
             }
         });
+
         logoutButton=(Button) findViewById(R.id.signOut);
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
--- GymQuest/app/src/main/java/com/example/alejandro_luna/Monday.java
+++ GymQuest/app/src/main/java/com/example/alejandro_luna/Monday.java
@@ -1,27 +1,28 @@
 package com.example.alejandro_luna;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +30,7 @@ import java.util.List;
 public class Monday extends AppCompatActivity {
 
     private ListView mondayList,mondayAddRoutines;
+    private ImageView addRoutineImage;
     private static final String TAG = "Mondayyyy";
 
     @SuppressLint("WrongViewCast")
@@ -41,28 +43,49 @@ public class Monday extends AppCompatActivity {
 
         mondayList = findViewById(R.id.mondayRoutines);
         mondayAddRoutines=findViewById(R.id.mondayAddRoutines);
+        addRoutineImage=findViewById(R.id.addImage);
 
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         String currentUserEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
 
-        mondayList = findViewById(R.id.mondayRoutines);
 
-        //only works with users
-        CollectionReference routinesRef = db.collection("exercises");
 
-        routinesRef.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+        DatabaseReference routinesRef = FirebaseDatabase.getInstance().getReference().child("routines");
+
+        routinesRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                List<String> routines = new ArrayList<>();
-                for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
-                    String title = document.getId();
-                    routines.add(title);
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                List<String> routineTitles = new ArrayList<>(); // Create a list to store routine titles
+
+                for (DataSnapshot routineSnapshot : snapshot.getChildren()) { // Iterate over each routine snapshot in the data snapshot
+                    String title = routineSnapshot.getKey(); // Get the value of the "title" field
+                    routineTitles.add(title); // Add the title to the list of routine titles
                 }
-                ArrayAdapter<String> adapter = new ArrayAdapter<>(Monday.this, android.R.layout.simple_list_item_1, routines);
-                mondayAddRoutines.setAdapter(adapter);
+
+                // Create a ListView adapter with the list of routine titles
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(Monday.this, android.R.layout.simple_list_item_1, routineTitles);
+                mondayAddRoutines.setAdapter(adapter); // Set the adapter to your ListView
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e(TAG, "Error retrieving routines: " + error.getMessage());
             }
         });
+
+        // Establece un Listener de elemento de lista en tu ListView
+                mondayAddRoutines.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        // Obtiene el elemento de la lista que ha sido seleccionado
+                        String selectedRoutine = (String) parent.getItemAtPosition(position);
+
+                        // Realiza la acción deseada con el elemento seleccionado
+                        // Por ejemplo, podrías abrir una nueva actividad y pasar el elemento seleccionado como parámetro
+
+                    }
+                });
 
 
 
