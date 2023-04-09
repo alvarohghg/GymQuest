a17ad43d867222b2753cf6eb38a19c8135c6912f
alvarohghg <lvaro.berjillos@gmail.com>
2023-04-09 15:43:05 +0100

    Feat: edit routine/show routines

--- GymQuest/app/src/main/java/com/example/gymquest/editRoutine.java
+++ GymQuest/app/src/main/java/com/example/gymquest/editRoutine.java
@@ -4,17 +4,15 @@ import static android.content.ContentValues.TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -22,45 +20,63 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import com.google.firebase.database.Query;
 
-public class planification extends AppCompatActivity {
+
+public class editRoutine extends AppCompatActivity {
     private Button Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday;
-    private TextView planificationRoutine,planificationDuration;
-    private ListView planificationExercises;
-    private ImageView planificationEdit;
-    @SuppressLint("MissingInflatedId")
+    private TextView editRoutineCurrentRoutine;
+    private ListView editRoutineList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_planification);
-        Monday=(Button) findViewById(R.id.planificationMonday);
-        Tuesday=(Button)findViewById(R.id.planificationTuesday);
-        Wednesday=(Button)findViewById(R.id.planificationWednesday);
-        Thursday=(Button)findViewById(R.id.planificationThursday);
-        Friday=(Button)findViewById(R.id.planificationFriday);
-        Saturday=(Button)findViewById(R.id.planificationSaturday);
-        Sunday=(Button)findViewById(R.id.planificationSunday);
+        setContentView(R.layout.activity_edit_routine);
+        Monday=(Button) findViewById(R.id.editRoutineMonday);
+        Tuesday=(Button)findViewById(R.id.editRoutineTuesday);
+        Wednesday=(Button)findViewById(R.id.editRoutineWednesday);
+        Thursday=(Button)findViewById(R.id.editRoutineThursday);
+        Friday=(Button)findViewById(R.id.editRoutineFriday);
+        Saturday=(Button)findViewById(R.id.editRoutineSaturday);
+        Sunday=(Button)findViewById(R.id.editRoutineSunday);
+        editRoutineCurrentRoutine=(TextView)findViewById(R.id.editRoutineCurrentRoutine);
+        editRoutineList=(ListView)findViewById(R.id.editRoutineList);
 
+        //show all the routines available
+        DatabaseReference routinesRef = FirebaseDatabase.getInstance().getReference().child("routines");
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        String currentUserEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
-        planificationRoutine=findViewById(R.id.planificationRoutine);
-        planificationDuration=findViewById(R.id.planificationDuration);
-        planificationExercises=findViewById(R.id.editRoutineList);
-
-        planificationEdit=(ImageView)findViewById(R.id.planificationEdit);
-        planificationEdit.setOnClickListener(new View.OnClickListener() {
+        routinesRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(planification.this, editRoutine.class));
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                List<String> routineTitles = new ArrayList<>(); // Create a list to store routine titles
+
+                for (DataSnapshot routineSnapshot : snapshot.getChildren()) { // Iterate over each routine snapshot in the data snapshot
+                    String title = routineSnapshot.getKey(); // Get the value of the "title" field
+                    routineTitles.add(title); // Add the title to the list of routine titles
+                }
+
+                // Create a ListView adapter with the list of routine titles
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(editRoutine.this, android.R.layout.simple_list_item_1, routineTitles);
+                editRoutineList.setAdapter(adapter); // Set the adapter to your ListView
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e(TAG, "Error retrieving routines: " + error.getMessage());
             }
         });
+
+
+
 
         Monday.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -106,6 +122,7 @@ public class planification extends AppCompatActivity {
         });
 
 
+
     }
     private void displayRoutineForDay(String day) {
         // Show the user's routines
@@ -119,32 +136,8 @@ public class planification extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
                     String currentRoutine = (String) userSnapshot.child(day.toLowerCase()).getValue();
-                    planificationRoutine.setText(currentRoutine);
-                    if (currentRoutine != null) {
-                        // Find the routine in the "routines" collection
-                        DatabaseReference routinesRef = FirebaseDatabase.getInstance().getReference().child("routines");
-                        Query routineQuery = routinesRef.orderByChild("title").equalTo(currentRoutine);
+                    editRoutineCurrentRoutine.setText(currentRoutine);
 
-                        routineQuery.addListenerForSingleValueEvent(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                for (DataSnapshot routineSnapshot : dataSnapshot.getChildren()) {
-                                    String duration = routineSnapshot.child("duration").getValue(String.class);
-                                    String exercisesString = routineSnapshot.child("exercises").getValue(String.class);
-                                    List<String> exercises = Arrays.asList(exercisesString.split(","));
-                                    ArrayAdapter<String> adapter = new ArrayAdapter<>(planification.this, android.R.layout.simple_list_item_1, exercises);
-                                    planificationExercises.setAdapter(adapter);
-                                    planificationDuration.setText(duration);
-                                }
-                            }
-
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError databaseError) {
-                                Log.e(TAG, "Error retrieving routines: " + databaseError.getMessage());
-                            }
-                        });
-                        break;
-                    }
                 }
             }
 
@@ -154,5 +147,4 @@ public class planification extends AppCompatActivity {
             }
         });
     }
-
 }
